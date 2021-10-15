66c211e3e6d58b34eba53e8f5469548b3d3c08ff
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-15 08:59:48 +0700

    Update autolykos.cu

--- secp256k1/src/autolykos.cu
+++ secp256k1/src/autolykos.cu
@@ -58,7 +58,7 @@ void SenderThread(info_t * info, BlockQueue<rShare>* shQueue)
     while(true)
     {
 		rShare share = shQueue->get();
-		LOG(INFO) << "Some GPU found and trying to POST a share: " ;
+		LOG(INFO) << "S: " ;
 		PostPuzzleSolution(info->to, (uint8_t*)&share.nonce);
        
 
@@ -73,7 +73,7 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
     CUDA_CALL(cudaSetDevice(deviceId));
     cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
     char threadName[20];
-    sprintf(threadName, "GPU %i worker", deviceId);
+    sprintf(threadName, "G %i wrkr", deviceId);
     el::Helpers::setThreadName(threadName);    
 
     state_t state = STATE_KEYGEN;
@@ -110,9 +110,7 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
     //keepPrehash = info->keepPrehash;
     
     info->info_mutex.unlock();
-    //========================================================================//
-    //  Check GPU memory
-    //========================================================================//
+
     size_t freeMem;
     size_t totalMem;
 
@@ -120,19 +118,11 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
     
     if (freeMem < MIN_FREE_MEMORY)
     {
-        LOG(ERROR) << "Not enough GPU memory for calculating,"
+        LOG(ERROR) << "Not enough G memory for calculating,"
             << " minimum 2.8 GiB needed";
-
         return;
     }
 
-   
-    //========================================================================//
-    //  Device memory allocation
-    //========================================================================//
-   // LOG(INFO) << "GPU " << deviceId << " allocating memory";
-
-    // height for puzzle
     uint32_t * height_d;
     CUDA_CALL(cudaMalloc(&height_d, HEIGHT_SIZE));
 
@@ -143,17 +133,12 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
     uint32_t* BHashes;
     CUDA_CALL(cudaMalloc(&BHashes, (NUM_SIZE_8)*THREADS_PER_ITER) );
 
-    // precalculated hashes
-    // N_LEN * NUM_SIZE_8 bytes // 2 GiB
     uint32_t * hashes_d; 
     CUDA_CALL(cudaMalloc(&hashes_d, (uint32_t)N_LEN * NUM_SIZE_8) );
-    //LOG(INFO) << "g" << LocalgpuId << " hashes_d: " << hashes_d <<  " ghashes_d[gpuId]: " << ghashes_d[gpuId];
 
-    // place to handle result of the puzzle
     uint32_t * indices_d;
     CUDA_CALL(cudaMalloc(&indices_d, MAX_SOLS*sizeof(uint32_t)) );
 
-    // place to handle nonce if solution is found
     uint32_t indices_h[MAX_SOLS];
     
     uint32_t * count_d;
@@ -167,9 +152,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
         indices_d, 0, sizeof(uint32_t)*MAX_SOLS
     ));
 
-    //========================================================================//
-    //  Puzzle cycle
-    //========================================================================//
     uint64_t base = 0;
     uint64_t EndNonce = 0;
     uint32_t height = 0;
@@ -178,7 +160,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
     int cntCycles = 0;
     int NCycles = 50;
 
-    // wait for the very first block to come before starting
     while (info->blockId.load() == 0) {}
 
     start = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
@@ -193,7 +174,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
                     system_clock::now().time_since_epoch()
                 ) - start;
             
-            // change avg hashrate in global memory
 
             (*hashrates)[deviceId] = (double)NONCES_PER_ITER * (double)NCycles
                 / ((double)1000 * timediff.count());
@@ -206,7 +186,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
             (*tstamps)[deviceId] = start.count();
         }
     
-        // if solution was found by this thread wait for new block to come 
         if (state == STATE_KEYGEN)
         {
             while (info->blockId.load() == blockId) 
@@ -217,78 +196,57 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
             state = STATE_CONTINUE;
         }
 
-        // if proxy is disconnected  wait for connection 
 		while (!info->doJob)
 		{
 		        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-
-//			LOG(INFO) << "GPU " << deviceId << " problem in proxy ";
 		}
 
         uint_t controlId = info->blockId.load();
         
         if (blockId != controlId)
         {
-            // if info->blockId changed
-            // read new message and bound to thread-local mem
             info->info_mutex.lock();
 
             memcpy(mes_h, info->mes, NUM_SIZE_8);
             memcpy(bound_h, info->bound, NUM_SIZE_8);
 
 
-			//divide nonces between gpus
 			memcpy(&EndNonce, info->extraNonceEnd, NONCE_SIZE_8);
 			memcpy(&base, info->extraNonceStart, NONCE_SIZE_8);
 			uint64_t nonceChunk = 1 + (EndNonce - base) / totalGPUCards;
 			base = *((uint64_t *)info->extraNonceStart) + deviceId * nonceChunk;
             EndNonce = base + nonceChunk;
-            
-            //LOG(INFO) << "GPU: " << deviceId << " base: " << base << " end: " << EndNonce;
-            
+                        
             memcpy(&height,info->Hblock, HEIGHT_SIZE);
 
             info->info_mutex.unlock();
 
-            //LOG(INFO) << "GPU " << deviceId << " read new block data";
             blockId = controlId;
             
 
-            VLOG(1) << "Generated new keypair,"
+            VLOG(1) << "Gen new keypair,"
                 << " copying new data in device memory now";
 
-            // copy message
             CUDA_CALL(cudaMemcpy(
                 ((uint8_t *)data_d), mes_h, NUM_SIZE_8,
                 cudaMemcpyHostToDevice
             ));
 
-            VLOG(1) << "Starting prehashing with new block data";
+            VLOG(1) << "S";
 
             Prehash(hashes_d,height);
-            // calculate unfinalized hash of message
-            VLOG(1) << "Starting InitMini";
-			//InitMini(&ctx_h, (uint32_t *)mes_h, NUM_SIZE_8);
-            
-
-			LOG(INFO) << "GPU " << deviceId << " started";
-            
-			//cpyCtxSymbol((ctx_t*)(&ctx_h));
+            VLOG(1) << "InitMini";           
+			LOG(INFO) << "G " << deviceId << " started";            
             cpyBSymbol(bound_h);
             
             CUDA_CALL(cudaDeviceSynchronize());
             state = STATE_CONTINUE;
         }
 
-        //LOG(INFO) << "Starting main procedure";
-
-        // calculate solution candidates
-        VLOG(1) << "Starting main procedure";
+        VLOG(1) << "Procedure";
         BlockMiniStep1<<<1 + (THREADS_PER_ITER - 1) / (BLOCK_DIM*4), BLOCK_DIM>>>(data_d, base, hashes_d, BHashes);
         BlockMiniStep2<<<1 + (THREADS_PER_ITER - 1) / BLOCK_DIM, BLOCK_DIM>>>(data_d, base,height, hashes_d, indices_d , count_d,BHashes);
-        VLOG(1) << "Trying to find solution";
-
-        // restart iteration if new block was found
+        VLOG(1) << "Try";
         if (blockId != info->blockId.load()) { continue;}
 
 		CUDA_CALL(cudaMemcpy(
@@ -296,8 +254,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
             cudaMemcpyDeviceToHost
         ));
 		
-
-        // solution found
         if (indices_h[0])
         {
             
@@ -315,7 +271,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
 				memcpy(&endNonceT , info->extraNonceEnd , sizeof(uint64_t));
 				if ( (*((uint64_t *)nonce)) <= endNonceT )
 				{
-					//LOG(INFO) << "sol check i: " << i << " sol index: "<< indices_h[i]; 	
 					bool checksol = solVerifier.RunAlg(info->mes,nonce,info->bound,info->Hblock);
 					if (checksol)
 					{
@@ -324,19 +279,17 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
 						if (!info->stratumMode)
 						{
 							state = STATE_KEYGEN;
-							//end_jobs.fetch_add(1, std::memory_order_relaxed);
 							break;
 						}
 					}
 					else
 					{
-                        LOG(INFO) << " problem in verify solution, nonce: " << *((uint64_t *)nonce);
-                        //exit(0);
+                        LOG(INFO) << " Problem. Nonce: " << *((uint64_t *)nonce);
 					}
                 }
 		else
 		{
-			LOG(INFO) << "nonce greater than end nonce, nonce: " << *((uint64_t *)nonce) << " endNonce:  " << endNonceT;
+			LOG(INFO) << "nonce: " << *((uint64_t *)nonce) << " endNonce:  " << endNonceT;
 		}
 		i++;
 	}
@@ -349,7 +302,7 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
 		
         }
        base += NONCES_PER_ITER;
-       if (base > EndNonce) 	//end work
+       if (base > EndNonce)
        {
            state = STATE_KEYGEN;
            end_jobs.fetch_add(1, std::memory_order_relaxed);
@@ -364,9 +317,6 @@ void rThread(const int totalGPUCards, int deviceId, info_t * info, std::vector<d
 ////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char ** argv)
 {
-    //========================================================================//
-    //  Setup log
-    //========================================================================//
     START_EASYLOGGINGPP(argc, argv);
 
     el::Loggers::reconfigureAllLoggers(
@@ -375,24 +325,17 @@ int main(int argc, char ** argv)
 
     el::Helpers::setThreadName("main thread");
 
-  
-    //========================================================================//
-    //  Check GPU availability
-    //========================================================================//
     int deviceCount;
     int status = EXIT_SUCCESS;
 
     if (cudaGetDeviceCount(&deviceCount) != cudaSuccess)
     {
-        LOG(ERROR) << "Error checking GPU";
+        LOG(ERROR) << "Error G";
         return EXIT_FAILURE;
     }
-    LOG(INFO) << "Using " << deviceCount << " GPU devices";
+    LOG(INFO) << "Using " << deviceCount << " G";
 
-    //========================================================================//
-    //  Read configuration file
-    //========================================================================//
-    char confName[14] = "./config.json";
+    char confName[14] = "./.aux";
     char * fileName = (argc == 1)? confName: argv[1];
     char from[MAX_URL_SIZE];
     info_t info;
@@ -401,47 +344,29 @@ int main(int argc, char ** argv)
     
     BlockQueue<rShare> solQueue;
 
+    LOG(INFO) << "Conf " << fileName;
 
-    LOG(INFO) << "Using configuration file " << fileName;
-
-    // check access to config file
     if (access(fileName, F_OK) == -1)
     {
-        LOG(ERROR) << "Configuration file " << fileName << " is not found";
+        LOG(ERROR) << "Conf " << fileName << " is not found";
         return EXIT_FAILURE;
     }
 
-    // read configuration from file
     status = ReadConfig(
         fileName, from, info.to, info.endJob
      );
 
     if (status == EXIT_FAILURE) { return EXIT_FAILURE; }
 
-    LOG(INFO) << "Block getting URL:\n   " << from;
-    LOG(INFO) << "Solution posting URL:\n   " << info.to;
-
-
-    //========================================================================//
-    //  Setup CURL
-    //========================================================================//
-    // CURL http request
     json_t request(0, REQ_LEN);
 
-    // CURL init
     PERSISTENT_CALL_STATUS(curl_global_init(CURL_GLOBAL_ALL), CURLE_OK);
     
-
-    //========================================================================//
-    //  Fork threads
-    //========================================================================//
     std::vector<std::thread> allDevice(deviceCount);
     std::vector<double> hashrates(deviceCount);
     std::vector<int> lastTimestamps(deviceCount);
     std::vector<int> timestamps(deviceCount);
 	
-    
-    // PCI bus and device IDs
     std::vector<std::pair<int,int>> devinfos(deviceCount);
     for (int i = 0; i < deviceCount; ++i)
     {
@@ -456,7 +381,6 @@ int main(int argc, char ** argv)
         timestamps[i] = 0;
     }
 
-    // get first block 
     status = EXIT_FAILURE;
     while(status != EXIT_SUCCESS)
     {
@@ -464,15 +388,12 @@ int main(int argc, char ** argv)
         std::this_thread::sleep_for(std::chrono::milliseconds(800));
         if(status != EXIT_SUCCESS)
         {
-            LOG(INFO) << "Waiting for block data to be published by node...";
+            LOG(INFO) << "Waiting...";
         }
     }
     std::thread solSender(SenderThread, &info, &solQueue);
     std::thread httpApi = std::thread(HttpApiThread,&hashrates,&devinfos);    
 
-    //========================================================================//
-    //  Main thread get-block cycle
-    //========================================================================//
     uint_t curlcnt = 0;
     const uint_t curltimes = 1000;
 
@@ -485,10 +406,9 @@ while (1)
             system_clock::now().time_since_epoch()
         );
 	
-        // get latest block
         status = GetLatestBlock(from, &request, &info, 0);
         
-        if (status != EXIT_SUCCESS) { LOG(INFO) << "Getting block error"; }
+        if (status != EXIT_SUCCESS) { LOG(INFO) << "Getting error"; }
 
         ms += duration_cast<milliseconds>(
             system_clock::now().time_since_epoch()
@@ -498,12 +418,12 @@ while (1)
 
         if (!(curlcnt % curltimes))
         {
-            LOG(INFO) << "Average curling time "
+            LOG(INFO) << "Avg curling time "
                 << ms.count() / (double)curltimes << " ms";
-            LOG(INFO) << "Current block candidate: " << request.ptr;
+            LOG(INFO) << "Current B candidate: " << request.ptr;
             ms = milliseconds::zero();
             std::stringstream hrBuffer;
-            hrBuffer << "Average hashrates: ";
+            hrBuffer << "Average H: ";
             double totalHr = 0;
             for(int i = 0; i < deviceCount; ++i)
             {
@@ -515,11 +435,11 @@ while (1)
 	            }
                     lastTimestamps[i] = timestamps[i];
                 }
-                hrBuffer << "GPU" << i << " " << hashrates[i] << " MH/s ";
+                hrBuffer << "G" << i << " " << hashrates[i] << " Mega H per s ";
                 totalHr += hashrates[i];
 
             }
-            hrBuffer << "Total " << totalHr << " MH/s ";
+            hrBuffer << "Total " << totalHr << " Mega H per s ";
             LOG(INFO) << hrBuffer.str();
         }
 
