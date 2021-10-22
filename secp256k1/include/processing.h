307d33fec8b9f94dab6c237dd9720bf5e1e2607a
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-22 09:35:56 +0700

    Update processing.h

--- secp256k1/include/processing.h
+++ secp256k1/include/processing.h
@@ -2,13 +2,8 @@
 #ifndef PROCESSING_H
 #define PROCESSING_H
 
-/*******************************************************************************
-    PROCESSING -- Puzzle cycle execution support
-*******************************************************************************/
-
 #include "definitions.h"
 
-// read config file
 int ReadConfig(
     const char * fileName,
     char * from,
@@ -16,12 +11,10 @@ int ReadConfig(
     char * endJob
 );
 
-// print public key
 int PrintPublicKey(const char * pkstr, char * str);
 
 int PrintPublicKey(const uint8_t * pk, char * str);
 
-// print Autolukos puzzle solution
 int PrintPuzzleSolution(
     const uint8_t * nonce,
     const uint8_t * sol,
