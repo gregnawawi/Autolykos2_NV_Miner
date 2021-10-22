a7f2ab05ac23e2c6f4f72e7e677f66a25a43c13f
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-22 09:35:27 +0700

    Update prehash.h

--- secp256k1/include/prehash.h
+++ secp256k1/include/prehash.h
@@ -3,16 +3,11 @@
 
 #include "definitions.h"
 
-// first iteration of hashes precalculation
 __global__ void InitPrehash(
-    // height
     const uint32_t height,
-    // hashes
     uint32_t *hashes);
 
-// precalculate hashes
 int Prehash(
-    // hashes
     uint32_t *hashes,
     uint32_t height);
 
