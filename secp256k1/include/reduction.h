ab6cad753790f2b50c5f0eac2d57f0d19bfa1638
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-22 09:37:13 +0700

    Update reduction.h

--- secp256k1/include/reduction.h
+++ secp256k1/include/reduction.h
@@ -1,18 +1,10 @@
 #ifndef REDUCTION_H
 #define REDUCTION_H
 
-/*******************************************************************************
-
-    REDUCTION -- Identification of Autolykos puzzle solution 
-
-*******************************************************************************/
-
 #include "definitions.h"
 
-// find smallest power of two not lesser then given number
 uint32_t CeilToPower(uint32_t x);
 
-// find non zero item in block
 template<uint32_t blockSize>
 __global__ void BlockNonZero(
     uint32_t * in,
@@ -20,7 +12,6 @@ __global__ void BlockNonZero(
     uint32_t * out
 );
 
-// sum all elements in a block
 template<uint32_t blockSize>
 __global__ void BlockSum(
     uint32_t * in,
@@ -28,7 +19,6 @@ __global__ void BlockSum(
     uint32_t * out
 );
 
-// find non zero item in each block of array
 void ReduceNonZero(
     uint32_t * in,
     uint32_t inlen,
@@ -37,7 +27,6 @@ void ReduceNonZero(
     uint32_t blockSize
 );
 
-// find sum of all elements in each block of array
 void ReduceSum(
     uint32_t * in,
     uint32_t inlen,
@@ -46,14 +35,12 @@ void ReduceSum(
     uint32_t blockSize
 );
 
-// find non zero item in array
 uint32_t FindNonZero(
     uint32_t * data,
     uint32_t * aux,
     uint32_t inlen
 );
 
-// find sum of all elements in array
 uint32_t FindSum(
     uint32_t * data,
     uint32_t * aux,
