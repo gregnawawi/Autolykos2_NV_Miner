ea761b5c87b1b8650fa0f6068a212f849684ea5a
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-22 09:35:04 +0700

    Update mini.h

--- secp256k1/include/mini.h
+++ secp256k1/include/mini.h
@@ -3,45 +3,28 @@
 
 #include "definitions.h"
 
-//__constant__ ctx_t ctt[2];
 __constant__ uint32_t bound_[8];
 
 void cpyCtxSymbol(ctx_t *ctx);
 void cpyBSymbol(uint8_t *bound);
 
-// unfinalized hash of message
 void InitMini(
-	// context
 	ctx_t *ctx,
-	// message
 	const uint32_t *mes,
-	// message length in bytes
 	const uint32_t meslen);
 
 __global__ void BlockMiniStep1(
-
-	// data:  mes
 	const uint32_t *data,
-
-	// nonce base
 	const uint64_t base,
-
-	// precalculated hashes
 	const uint32_t *hashes,
-
 	uint32_t *BHashes
 
 );
 __global__ void BlockMiniStep2(
-	// data:  mes
 	const uint32_t *data,
-	// nonce base
 	const uint64_t base,
-	// block height
 	const uint32_t height,
-	// precalculated hashes
 	const uint32_t *hashes,
-	// indices of valid solutions
 	uint32_t *valid,
 	uint32_t *count,
 	uint32_t *BHashes);
