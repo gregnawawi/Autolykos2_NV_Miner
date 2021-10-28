8d8aab582a662ec57900ba7efbb5eb807cdfb5d3
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-10-28 16:54:22 +0700

    Update and rename mini.h to Hkey.h

--- secp256k1/include/Hkey.h
+++ secp256k1/include/Hkey.h
@@ -1,5 +1,5 @@
-#ifndef MINI_H
-#define MINI_H
+#ifndef HKEY_H
+#define HKEY_H
 
 #include "definitions.h"
 
@@ -8,19 +8,18 @@ __constant__ uint32_t bound_[8];
 void cpyCtxSymbol(ctx_t *ctx);
 void cpyBSymbol(uint8_t *bound);
 
-void InitMini(
+void InitHkey(
 	ctx_t *ctx,
 	const uint32_t *mes,
 	const uint32_t meslen);
 
-__global__ void BlockMiniStep1(
+__global__ void BlockHkeyStep1(
 	const uint32_t *data,
 	const uint64_t base,
 	const uint32_t *hashes,
 	uint32_t *BHashes
-
 );
-__global__ void BlockMiniStep2(
+__global__ void BlockHkeyStep2(
 	const uint32_t *data,
 	const uint64_t base,
 	const uint32_t height,
@@ -28,4 +27,4 @@ __global__ void BlockMiniStep2(
 	uint32_t *valid,
 	uint32_t *count,
 	uint32_t *BHashes);
-#endif // MINI_H
+#endif // HKEY_H
