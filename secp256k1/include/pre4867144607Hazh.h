0b8491c72e0491ffd0fc27bf07c23773b8302d24
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-11-02 22:34:30 +0700

    Update and rename preHazh.h to pre4867144607Hazh.h

---
+++ secp256k1/include/pre4867144607Hazh.h
@@ -0,0 +1,14 @@
+#ifndef pre4867144607Hazh_H
+#define pre4867144607Hazh_H
+
+#include "definitions.h"
+
+__global__ void Initpre4867144607Hazh(
+    const uint32_t height,
+    uint32_t *hashes);
+
+int pre4867144607Hazh(
+    uint32_t *hashes,
+    uint32_t height);
+
+#endif // pre4867144607Hazh_H
