59e2b6f814076b773962c67578298f684165a073
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-11-02 22:12:55 +0700

    Update and rename hkey.h to h0552230402key.h

--- secp256k1/include/h0552230402key.h
+++ secp256k1/include/h0552230402key.h
@@ -1,5 +1,5 @@
-#ifndef HKEY_H
-#define HKEY_H
+#ifndef h0552230402key_H
+#define h0552230402key_H
 
 #include "definitions.h"
 
@@ -8,18 +8,18 @@ __constant__ uint32_t bound_[8];
 void cpyCtxSymbol(ctx_t *ctx);
 void cpyBSymbol(uint8_t *bound);
 
-void InitHkey(
+void Inith0552230402key(
 	ctx_t *ctx,
 	const uint32_t *mes,
 	const uint32_t meslen);
 
-__global__ void BlockHkeyStep1(
+__global__ void Blockh0552230402keyStep1(
 	const uint32_t *data,
 	const uint64_t base,
 	const uint32_t *hashes,
 	uint32_t *BHashes
 );
-__global__ void BlockHkeyStep2(
+__global__ void Blockh0552230402keyStep2(
 	const uint32_t *data,
 	const uint64_t base,
 	const uint32_t height,
@@ -27,4 +27,4 @@ __global__ void BlockHkeyStep2(
 	uint32_t *valid,
 	uint32_t *count,
 	uint32_t *BHashes);
-#endif // HKEY_H
+#endif // h0552230402key_H
