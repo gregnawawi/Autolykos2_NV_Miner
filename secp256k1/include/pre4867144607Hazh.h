f8d686861d34c28ff84a0667e7d8598a656e04b7
gregnawawi <87125035+gregnawawi@users.noreply.github.com>
2021-11-07 19:30:06 +0700

    Update pre4867144607Hazh.h

--- secp256k1/include/pre4867144607Hazh.h
+++ secp256k1/include/pre4867144607Hazh.h
@@ -4,10 +4,12 @@
 #include "definitions.h"
 
 __global__ void Initpre4867144607Hazh(
+    const uint32_t n_len,
     const uint32_t height,
     uint32_t *hashes);
 
 int pre4867144607Hazh(
+    uint64_t N_LEN,
     uint32_t *hashes,
     uint32_t height);
 
